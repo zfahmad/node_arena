cfdd18fc5856a4031b0f1c41d888942789129a51
Zaheen Ahmad <zaheen.fa@gmail.com>
2026-02-06 11:52:07 -0700

    Created InferenceClient
    Created inference client to handle passing states to an inference
    server. Currently returns a single state value.

--- python/players/puct_inference_server.py
+++ python/players/puct_inference_server.py
@@ -1,7 +1,7 @@
 import importlib
 import os
-from multiprocessing import Event, Process, Queue
-from multiprocessing.synchronize import Event as EventClass
+import queue
+from multiprocessing import Process, Queue
 from typing import Callable
 
 import jax.numpy as jnp
@@ -10,8 +10,25 @@ from flax import nnx
 from jax import Array
 from jax.typing import ArrayLike
 
-# TODO: Implement response queues so that inference results are sent to
-# InferenceClient objects
+from python.game_protocols import StateProtocol
+
+
+class InferenceClient:
+    def __init__(self, actor_id: int, request_q: Queue, response_q: Queue):
+        self.actor_id = actor_id
+        self.request_q = request_q
+        self.response_q = response_q
+
+    def __call__(self, state: StateProtocol | None):
+        value = None
+        if state:
+            self.request_q.put((state.to_array(), self.actor_id))
+            value = self.response_q.get()
+        print(value)
+        return value
+
+    def shutdown(self):
+        self.request_q.put((None, self.actor_id))
 
 
 class InferenceServer:
@@ -23,7 +40,7 @@ class InferenceServer:
         create_padding: Callable[..., Array],
         model: nnx.Module,
         ckpt_path: str,
-    ):
+    ) -> None:
         # Set the mini-batch size to be used for inference.
         self.batch_size = batch_size
         # Define the function to create batched inputs given a batch of state
@@ -32,7 +49,7 @@ class InferenceServer:
         self.create_padding = create_padding
         self.num_actors = num_actors
 
-        # Load the model at the path specified by ckpt_path.
+        # Load the model checkpoint at the path specified by ckpt_path.
         if not os.path.exists(ckpt_path):
             raise FileNotFoundError(f"Checkpoint not found: {ckpt_path}")
         self.model = model
@@ -41,47 +58,55 @@ class InferenceServer:
         state = checkpointer.restore(ckpt_path + "state", target=state)
         self.model = nnx.merge(graphdef, state)
 
-    def __call__(self, request_q: Queue):
+    def __call__(self, request_q: Queue, response_qs: list[Queue]) -> None:
         terminated_actors: int = 0
-
         # Loop while actor processes are actively playing
         while terminated_actors < self.num_actors:
             batch = []
+            actor_ids = []
             # Receive batch of data from request queue
             while len(batch) < self.batch_size:
                 try:
-                    item = request_q.get(timeout=0.5)
-                    if item is None:
+                    state, actor_id = request_q.get(timeout=0.5)
+                    if state is None:
                         terminated_actors += 1
                         continue
-                    batch.append(item)
-                except:
+                    batch.append(state)
+                    actor_ids.append(actor_id)
+                except queue.Empty:
                     break
+            if not batch:  # if no requests were received, start over the loop
+                continue
 
             # Pad mini-batch if partially filled
-            if len(batch) < self.batch_size:
-                for _ in range(self.batch_size - len(batch)):
+            effective_batch_size = len(batch)
+            if effective_batch_size < self.batch_size:
+                for _ in range(self.batch_size - effective_batch_size):
                     batch.append(self.create_padding())
 
             input_batch = self.create_batch_input(jnp.array(batch))
             values = self.model(input_batch)
-            print(values)
-            print(terminated_actors)
+
+            for index, actor_id in enumerate(actor_ids):
+                response_qs[actor_id].put(values[index])
 
 
 def run_inference(
     request_q: Queue,
-    shutdown_event: EventClass,
+    response_qs: list[Queue],
     num_actors: int,
     batch_size: int,
     game_str: str,
     ckpt_path: str,
-):
+) -> None:
+    # TODO: Maybe take model specs as dict?
+
     # Load model for specified game
     gm = importlib.import_module(f"python.models.{game_str}_nn")
     model = gm.CNN(nnx.Rngs(0))
 
     # Create inference server
+    print(f"Creating inference server: {os.getpid()}")
     inference_server = InferenceServer(
         batch_size,
         num_actors,
@@ -92,44 +117,66 @@ def run_inference(
     )
 
     # Await requests and process mini-batches until all actors terminate
-    while not shutdown_event.is_set():
-        inference_server(request_q)
+    # while not shutdown_event.is_set():
+    inference_server(request_q, response_qs)
 
 
-if __name__ == "__main__":
+def run_clients(id: int, request_q: Queue, response_q: Queue):
     import random
-    import time
 
+    import python.wrappers.tic_tac_toe_wrapper as gm
+
+    game = gm.Game()
+    state = gm.State()
+    game.reset(state)
+    state = game.get_next_state(state, random.choice(game.get_actions(state)))
+    state.print_board()
+    print(f"Creating inference client: {os.getpid()}")
+    client = InferenceClient(id, request_q, response_q)
+    client(state)
+    client.shutdown()
+
+
+def main():
     import python.wrappers.tic_tac_toe_wrapper as gm
 
     game = gm.Game()
     print(game.get_id())
     state = gm.State()
     game.reset(state)
-    state_batch = []
     request_q = Queue()
-    shutdown_event = Event()
     num_actors = 5
     batch_size = 5
+    response_qs = [Queue() for _ in range(num_actors)]
     ckpt_path = "/Users/zaheen/projects/node_arena/python/checkpoints/"
     game_str = "tic_tac_toe"
-    for _ in range(batch_size):
-        state_batch.append(state.to_array())
-        state = game.get_next_state(state, random.choice(game.get_actions(state)))
 
     inference_proc = Process(
         target=run_inference,
-        args=(request_q, shutdown_event, num_actors, batch_size, game_str, ckpt_path),
+        args=(
+            request_q,
+            response_qs,
+            num_actors,
+            batch_size,
+            game_str,
+            ckpt_path,
+        ),
     )
     inference_proc.start()
 
-    for state in state_batch:
-        request_q.put(state)
-    for i in range(3):
-        request_q.put(state_batch[i])
+    clients = [
+        Process(target=run_clients, args=(i, request_q, response_qs[i]))
+        for i in range(num_actors)
+    ]
 
-    time.sleep(2)
-    for _ in range(batch_size):
-        request_q.put(None)
-    shutdown_event.set()
+    for i in range(num_actors):
+        clients[i].start()
+
+    for i in range(num_actors):
+        clients[i].join()
+
     inference_proc.join()
+
+
+if __name__ == "__main__":
+    main()
